990a0ee4d523a82784a1298a25c8ddd2dee7f384
munemo <john.chimbani@outlook.com>
2020-07-30 09:51:22 +0200

    4th test

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -9,5 +9,13 @@ describe('user can create a contact', () => {
 		cy.get('#company').type('Craft Academy')
 		cy.get('#notes').type('Awesome coder')
 		cy.get('#twitter').type('@thomasochman')
+		cy.get('#submit').click()
+	})
+	it('displays a name of the new contact', () => {
+		cy.get('#contact-list').should('contain', 'Thomas')
+	})
+	
+	it('displays the phone number of the new contact', () => {
+		cy.get('#contact-list').should('contain', '0700 101010')
 	})
 })
