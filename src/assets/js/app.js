630659e21ded4393fc84c99cf42c665093177c2c
munemo <john.chimbani@outlook.com>
2020-08-02 22:11:07 +0200

    Fixing some issues

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,34 +23,34 @@ const  renderContacts = () => {
 	} else {
 	  div.innerHTML = '<p>You have no contacts in your address book</p>'
 	}
-    document.addEventListener('DOMContentLoaded', () => {
-        renderContacts()
-        const  contactForm = document.getElementById('new-contact-form')
-        contactForm.addEventListener('submit', event  => {
-            event.preventDefault()
-    
-            // 1. Read all the input fields and get their values
-            const { name, email, phone, company, notes, twitter } = contactForm.elements
-    
-            const  contact = {
-                name:  name.value,
-                email:  email.value,
-                phone:  phone.value,
-                company:  company.value,
-                notes:  notes.value,
-                twitter:  twitter.value,
-            }
-    
-            console.log(contact)
-    
-            let  contacts = JSON.parse(storage.getItem('contacts')) || []
-    
-            contacts.push(contact)
-    
-            // 2. Save them to our storage
-            storage.setItem('contacts', JSON.stringify(contacts))
-            renderContacts()
-            contactForm.reset()
-       })
-    })
 }
+document.addEventListener('DOMContentLoaded', () => {
+	renderContacts()
+	const  contactForm = document.getElementById('new-contact-form')
+	contactForm.addEventListener('submit', event  => {
+		event.preventDefault()
+
+		// 1. Read all the input fields and get their values
+		const { name, email, phone, company, notes, twitter } = contactForm.elements
+
+		const  contact = {
+			name:  name.value,
+			email:  email.value,
+			phone:  phone.value,
+			company:  company.value,
+			notes:  notes.value,
+			twitter:  twitter.value,
+		}
+
+		console.log(contact)
+
+		let  contacts = JSON.parse(storage.getItem('contacts')) || []
+
+		contacts.push(contact)
+
+		// 2. Save them to our storage
+		storage.setItem('contacts', JSON.stringify(contacts))
+		renderContacts()
+		contactForm.reset()
+   })
+})
